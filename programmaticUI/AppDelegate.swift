b3bdee1bcb22b7c7a4709ba5c92b646312c41e00
Alex <aalcazar064@gmail.com>
2021-10-18 23:00:00 -0700

    Initial Commit

--- programmaticUI/AppDelegate.swift
+++ programmaticUI/AppDelegate.swift
@@ -6,18 +6,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        if #available(iOS 13, *) {
-            
-        } else {
-            window = UIWindow()
-            setUpRootViewController()
-        }
-        
         return true
     }
 
@@ -34,10 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func setUpRootViewController() {
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
-    }
-    
+
+
 }
+
--- programmaticUI/ViewController.swift
+++ programmaticUI/ViewController.swift
@@ -3,11 +3,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .blue
+        let viewController2 = UIViewController()
+        viewController2.view.backgroundColor = .red
+        viewControllers = [viewController, viewController2]
     }
 
 
